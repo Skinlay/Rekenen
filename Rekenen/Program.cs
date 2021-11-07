6ad3bb6f2944c40050884e15b3b77ae3ed7d6e85
Caitlin Bond <401307@vistacollege.nl>
2021-11-07 16:29:54 +0100

    Add project files.

---
+++ Rekenen/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace Rekenen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exponents
+            Console.WriteLine("Hello World!");
+
+            double Result, Number1, Number2;
+
+            Number1 = Convert.ToInt32(Console.ReadLine());
+            Number2 = 3;
+
+            Result = Math.Pow(Number1, Number2);
+            Console.WriteLine(Result);
+
+        }
+    }
+}
