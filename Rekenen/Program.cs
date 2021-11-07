d68486650c714877f1a462dd50b5ca35684a35ff
Caitlin Bond <401307@vistacollege.nl>
2021-11-07 17:57:19 +0100

    kan de schuine zijde van een driehoek uitrekenen

--- Rekenen/Program.cs
+++ Rekenen/Program.cs
@@ -14,12 +14,12 @@ namespace Rekenen
             double Result, Number1, Number2;
 
             Number1 = Convert.ToInt32(Console.ReadLine());
-            Number2 = 3;
 
-            Result = Math.Pow(Number1, Number2);
+            Result = Math.Pow(Number1, 3);
             Console.WriteLine(Result);
             */
 
+            /*
             //Oppervlakte driehoek = (BASIS x HOOGTE)/2
 
             double oppervlakte, Number1, Number2;
@@ -32,6 +32,20 @@ namespace Rekenen
 
             oppervlakte = (Number1 * Number2) / 2;
             Console.WriteLine("de oppervlakte van de driehoek is: " + oppervlakte);
+            */
+
+            double ResultA, ResultB, ResultC, ResultD, zijdeA, zijdeB;
+
+            zijdeA = Convert.ToInt32(Console.ReadLine());
+            zijdeB = Convert.ToInt32(Console.ReadLine());
+
+            ResultA = Math.Pow(zijdeA, 2);
+            ResultB = Math.Pow(zijdeB, 2);
+            ResultC = ResultA + ResultB;
+            ResultD = Math.Sqrt(ResultC);
+
+            Console.WriteLine(ResultD);
+
 
         }
     }
